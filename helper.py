f856f995c2ce03c193a5b0c983ddeb8eaeb99cc1
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-09-15 18:12:01 +0800

    added login_required function.

--- helper.py
+++ helper.py
@@ -6,6 +6,20 @@ from flask import render_template  # redirect, render_template, request, session
 # from functools import wraps
 
 
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 def apology(message, code=400):
     """Render message as an apology to user."""
     def escape(s):
