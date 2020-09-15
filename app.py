99d0e528899eb1ad96af1f71a815be10d3824d04
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-09-15 18:23:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from psycopg2 import OperationalError
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from helper import apology, get_db
+from helper import apology, get_db, login_required
 
 
 # Configure application
