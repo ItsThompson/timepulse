a2c349487c6f8aa1cfb56099948387d609ea405e
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-09-17 19:23:26 +0800

    Should be good

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ connection = create_connection(
 @app.route("/", methods=["GET", "POST"])
 # @login_required
 def index():
-    if session.get("user_id") is None:
+    if session.get("user_id") is not None:
         return render_template("dashboard.html")
     return render_template("index.html")
 
@@ -150,6 +150,7 @@ def register():
 
 
 @app.route("/dashboard", methods=["GET", "POST"])
+@login_required
 def dashboard():
     return render_template("dashboard.html")
 
